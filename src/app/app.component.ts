1dd9192980910b1fbd947dae9fe6aa1cec8f74bf
Alex Oliveira <aroliveira@stefanini.com>
2021-04-06 16:05:45 -0300

    Primeira versão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'graficos';
+export class AppComponent implements OnInit {
+
+  dadosTabela = [
+    {name: 'Maria', value: 3},
+    {name: 'José', value: 5},
+  ];
+
+  dadosGrafico = [];
+
+  ngOnInit(): void {
+    this.refresh();
+  }
+
+  addLine(): void {
+    this.dadosTabela.push({name: '', value: null});
+    console.log('tabela');
+    console.log(this.dadosTabela);
+  }
+
+  refresh(): void {
+    this.dadosGrafico = this.dadosTabela
+      .filter(({name, value}) => name && value)
+      .map(({name, value}) => ({name: `${name}: ${value}`, value}));
+    console.log('grafico');
+    console.log(this.dadosGrafico);
+  }
 }
